e2e0fe38e0c9c8aa26875d2d22878f7feac905bf
stanislav <spg_stany@gmail.com>
2023-07-29 12:06:57 +0300

    data

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.869308bd5eb2ccc3f38ee28d198997a2.js"
+  "/precache-manifest.d15d773b2180c0b073c914cbb8b2c685.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "ds2-editor"});
